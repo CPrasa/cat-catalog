d83f77d160e23f446bc9c2d916c89e1361428442
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-06-29 01:25:29 +0530

    set the navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure this line is included
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CatBreedCard from './components/CatBreedCard';
+import CatBreedList from './components/CatBreedDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/breedCard" element={<CatBreedCard />} />
+        <Route path="/" element={<CatBreedList/>}> </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
